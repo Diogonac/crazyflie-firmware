e9a295ba90432f6850c45efa5d014c6d3e614f81
Diogonac <diogonac@al.insper.edu.br>
2021-08-17 09:32:54 -0300

    teste lab1

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -2,6 +2,10 @@
 #include "crazyflie.h"
 
 // Define all LEDs as digital output objects
+DigitalInOut BLUE(LED_BLUE_L);
+DigitalInOut RED_L(LED_RED_L);
+DigitalInOut RED_R(LED_RED_R);
+
 
 // Define all motors as PWM objects
 
@@ -9,8 +13,19 @@
 int main()
 {
     // Blink blue LED indicating inicialization (5 seconds)
-    
+    BLUE = 1;
+    wait(1);
+    BLUE = 0;
+    wait(1);
+    BLUE = 1;
+    wait(1);
+    BLUE = 0;
+    wait(1);
+    BLUE = 1;
+    wait(1);
+
     // Turn on red LEDs indicating motors are armed
+    
     
     // Test all motors with different frequencies (to make different noises)
     
