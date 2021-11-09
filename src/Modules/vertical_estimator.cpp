a61906adac06c74b255e44e2a3b3964914325b44
Diogonac <diogonac@al.insper.edu.br>
2021-11-09 08:46:39 -0300

    vertical estimator done

---
+++ src/Modules/vertical_estimator.cpp
@@ -0,0 +1,50 @@
+#include "vertical_estimator.h"
+
+// Class Constructor
+VerticalEstimator::VerticalEstimator() : range(E_SDA, E_SCL)
+{
+
+// Initial conditions
+z = 0.0;
+w = 0.0;
+
+
+}
+
+// Initialize class
+void VerticalEstimator ::init()
+{
+
+range.init();
+
+
+}
+
+// Predict vertical position and velocity from model
+void VerticalEstimator :: predict(float f_t)
+{
+ z = z + w * dt;
+ 
+ if(z > 0.05)
+ {
+ w = w + ((f_t/m)-g) * dt;
+ }
+ 
+
+}
+
+// Correct vertical position and velocity with measurement
+void VerticalEstimator :: correct(float phi , float theta)
+{
+
+range.read();
+
+if(range.d < 2.0)
+{
+    float z_m = cos(phi) * cos(theta) * range.d;
+
+    w = w + l1 * dt * (z_m - z);
+    z = z + l2 * dt * (z_m - z);
+}
+
+}
