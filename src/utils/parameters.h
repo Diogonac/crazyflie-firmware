fd8842c874896091c865cccc20f6b4d86fecefcc
Diogonac <diogonac@al.insper.edu.br>
2021-12-05 10:30:38 -0300

    mass correction

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -8,7 +8,7 @@ const float pi = 3.1416;
 const float g = 9.81;       // m/s^2
 
 // Quadcopter dimensions
-const float m = 30.0e-3;    // kg
+const float m = 32.0e-3;    // kg
 const float I_xx = 16.0e-6; // kg.m^2
 const float I_yy = 16.0e-6; // kg.m^2
 const float I_zz = 29.0e-6; // kg.m^2
